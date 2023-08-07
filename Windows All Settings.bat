68e517e2227cbb404ce861d98e0a2311486b5c58
SachaR <103992897+SachaRepo@users.noreply.github.com>
2023-08-08 00:15:21 +0200

    Add files via upload

---
+++ Windows All Settings.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+mkdir %USERPROFILE%\Desktop\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
+#Will create a control pannel logo on your desktop that has every windows settings all in one tab with even hidden settings.
+#Made by https://github.com/SachaRepo
